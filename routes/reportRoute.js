e5d3909aa3cb8eafd60045515b4517ed08a75aae
Arihant21 <raiarihant33@gmail.com>
2026-01-19 13:25:46 +0530

    Clean main branch: keep only README and gitignore

--- routes/reportRoute.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { uploadReport, getReports } from "../controllers/reportController.js";
-
-const router = express.Router();
-
-router.post("/upload", uploadReport);
-router.get("/patient/:id", getReports);
-
-export default router;
--- routes/userRoute.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { registerUser, loginUser } from "../controllers/userController.js";
-
-const router = express.Router();
-
-router.post("/signup", registerUser);
-router.post("/login", loginUser);
-
-export default router;
